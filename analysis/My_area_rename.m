fb8e72358f6556c6ff493cf20586b01946806899
Federica Rosselli <invernomuto@Federica-Rossellis-MacBook-Pro-3.local>
2013-05-13 18:30:41 +0200

    updates

---
+++ analysis/My_area_rename.m
@@ -0,0 +1,15 @@
+cd '/zocconasphys1/chronic_inv_rec/Tanks/Fede_Acute_Recording_12_4_2013/ANALYSED/BlockS-56/BL_2/My_Structure/25'
+
+files = dir(fullfile('*.mat'));
+neuronS = (numel(files))/2;
+
+for pappa = 1:neuronS
+    
+    load (['NEURON_', num2str(pappa), '.mat'])
+    
+    My_Neurons.Area=char('AM(V1b?)');
+    
+    save (['NEURON_', num2str(pappa), '.mat'], 'My_Neurons')
+    
+    
+end
