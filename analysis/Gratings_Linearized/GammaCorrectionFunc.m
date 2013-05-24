bb554c2b0bac9e4e2e405e57dc08d0a998e672b7
Federica Rosselli <invernomuto@Federica-Rossellis-MacBook-Pro-3.local>
2013-05-24 16:13:49 +0200

    added Gratings_Linearized

---
+++ analysis/Gratings_Linearized/GammaCorrectionFunc.m
@@ -0,0 +1,7 @@
+function y=GammaCorrectionFunc(x)
+       a =    0.0005322 ;
+       b =       2.328  ;
+        y = a*x.^b;
+%% gamma correctio for brightness ? black level 12 and gamma 1.8
+%Pixel Value=0:15:255
+%Absolute luminance cd/m2=[0.24 0.25 0.26 0.64 2.58 5.34 10.3 19.2 30.8 44.8 61.9 80.8 101.1 121.1 141.9 163.2 183.6 202.2]
---
+++ analysis/Gratings_Linearized/LinEstimBrigh13.m
@@ -0,0 +1,2 @@
+function y=LinEstimBrigh13(x)
+y=0.792*x+0.24;
